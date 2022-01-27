b02358144b13ac85c4f3ce843bc8eeb81ae46080
Spenser Black <spenserblack01@gmail.com>
2022-01-26 22:44:16 -0500

    Create svelte app

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import HelloWorld from './lib/HelloWorld.svelte';
+</script>
+
+<main>
+  <HelloWorld />
+</main>
+
+<style lang="stylus">
+  :root
+    font-family sans-serif
+
+  main
+    text-align center
+    padding 1em
+    margin 0 auto
+</style>
---
+++ src/lib/HelloWorld.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  let name = 'World';
+</script>
+
+<h1>Hello, {name}!</h1>

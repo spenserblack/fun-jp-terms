c72d03d0919833825f24dc0228e34c882cf97302
Spenser Black <spenserblack01@gmail.com>
2022-02-03 12:46:05 -0500

    Create Octicon component

---
+++ src/lib/Octicon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import octicons from '@primer/octicons';
+
+  export let name: keyof octicons | null = null;
+  $: icon = name == null ? null : octicons[name].toSVG();
+</script>
+
+{#if icon != null}
+  {@html icon}
+{/if}

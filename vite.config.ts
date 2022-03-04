123f58c7c7def4803f2014ad40d44689438b6d99
Spenser Black <spenserblack01@gmail.com>
2022-03-03 19:28:20 -0500

    Include `random/` in production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        random: resolve(__dirname, 'random/index.html'),
+      },
+    },
+  },
 });
